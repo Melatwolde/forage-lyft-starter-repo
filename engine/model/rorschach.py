1dc3298f9b74a538b7b4257404c873f6fb45a650
Melat woldegiorgis <123128037+Melatwolde@users.noreply.github.com>
2023-10-01 17:32:31 +0000

    first task refactor

--- engine/model/rorschach.py
+++ engine/model/rorschach.py
@@ -5,8 +5,18 @@ from engine.willoughby_engine import WilloughbyEngine
 
 class Rorschach(WilloughbyEngine):
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+
+        start_date = self.last_service_date
+        end_date = start_date.replace(year=start_date.year + 4)
+
+        while start_date < end_date:
+            mid_date = (start_date + end_date) // 2
+
+            if self.engine_should_be_serviced(mid_date):
+               return True
+            elif mid_date < datetime.today().date():
+               start_date = mid_date + 1
+            else:
+                end_date = mid_date - 1
+
+        return False
--- engine/model/thovex.py
+++ engine/model/thovex.py
@@ -5,8 +5,12 @@ from engine.capulet_engine import CapuletEngine
 
 class Thovex(CapuletEngine):
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+
+        for service_year in range(self.last_service_date.year, self.last_service_date.year + 5):
+            for service_month in range(1, 13):
+                for service_day in range(1, 32):
+                    service_date = datetime(service_year, service_month, service_day)
+                    if service_date <= datetime.today().date() and self.engine_should_be_serviced(service_date):
+                       return True
+        return False
+
